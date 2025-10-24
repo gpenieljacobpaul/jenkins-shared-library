7c9d24f1ea734a37bffdc2876a8a2984ec1781da
gpenieljacobpaul <penielunlock@gmail.com>
2025-10-24 16:54:03 +0530

    adding variables for image

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,8 @@
-def call() {
-    echo "building the docker image..."
+def call($Image) {
+    echo "building the docker image for the branch $BRANCH_NAME"
     withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t gpenieljacobpaul/docker-java-maven-app:2.0 .'
+        sh "docker build -t $Image ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push gpenieljacobpaul/docker-java-maven-app:2.0'
+        sh "docker push $Image"
     }
 }
